3163cf4b362dfcfc540c230d1d87d093a7c5b2a8
Alvin Noe Ladines <ladinesalvinnoe@gmail.com>
2023-12-08 15:41:17 +0100

    Use run schema plugin

--- databaseparsers/openkim/metainfo/openkim.py
+++ databaseparsers/openkim/metainfo/openkim.py
@@ -22,14 +22,17 @@ from nomad.metainfo import (  # pylint: disable=unused-import
     MSection, MCategory, Category, Package, Quantity, Section, SubSection, SectionProxy,
     Reference, JSON
 )
-from nomad.datamodel.metainfo import simulation
+import runschema.run  # pylint: disable=unused-import
+import runschema.calculation  # pylint: disable=unused-import
+import runschema.method  # pylint: disable=unused-import
+import runschema.system  # pylint: disable=unused-import
 import simulationworkflowschema
 
 
 m_package = Package()
 
 
-class Run(simulation.run.Run):
+class Run(runschema.run.Run):
 
     m_def = Section(validate=False, extends_base_section=True)
 
@@ -96,7 +99,7 @@ class Run(simulation.run.Run):
     )
 
 
-class System(simulation.system.System):
+class System(runschema.system.System):
 
     m_def = Section(validate=False, extends_base_section=True)
 
